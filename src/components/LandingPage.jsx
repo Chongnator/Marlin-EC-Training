7cdda37a8b6d88a77dabe64069bb53e6b0adb0c8
Juan Pablo Chong <juan.pablochong@snorkel.ai>
2025-12-04 17:21:32 -0600

    Update LandingPage.jsx

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -312,10 +312,11 @@ function LandingPage({ onNavigate }) {
                     <h4 style={{ fontSize: '1.125rem', fontWeight: 600, color: '#ffffff', marginBottom: '1rem', textAlign: 'center' }}>
                       Onboarding Video - check #ec-marlin-support-v2 for the password
                     </h4>
-         <ProtectedVideo 
-  src={"Marlin-EC-Training/public/media/videos/turorial-one.mp4"}
-  password="8bMf%@X+"/>
-  
+  <ProtectedVideo 
+  src="/Marlin-EC-Training/media/videos/turorial-one.mp4"
+  password="8bMf%@X+"
+/>
+
 
                   </div>
                 </div>
